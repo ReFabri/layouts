585c9a104a6cf06c774e631bd45726b1c7a3150f
Renato <renato.f.fabri@gmail.com>
2023-09-18 19:27:43 -0300

    Add navbar html

--- src/components/Layout2/Layout2.jsx
+++ src/components/Layout2/Layout2.jsx
@@ -1,10 +1,35 @@
 import "./layout2.css";
+import { Link } from "react-router-dom";
 
 const Layout2 = () => {
   return (
     <>
       <header className="primary-header">
-        <div className="container"></div>
+        <div className="container">
+          <Link to="#">
+            <img src="/layout1/logo.svg" alt="Manage Logo" />
+          </Link>
+          <nav className="primary-navigation">
+            <ul role="list" className="nav-list">
+              <li>
+                <Link to="#">Pricing</Link>
+              </li>
+              <li>
+                <Link to="#">Product</Link>
+              </li>
+              <li>
+                <Link to="#">About</Link>
+              </li>
+              <li>
+                <Link to="#">Careers</Link>
+              </li>
+              <li>
+                <Link to="#">Community</Link>
+              </li>
+            </ul>
+          </nav>
+          <button className="button">Get Started</button>
+        </div>
       </header>
     </>
   );
